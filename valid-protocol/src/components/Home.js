a43d468d366b7e6a2bd4bd44d5ba2fda3a5319ac
Bryan <bryanborck@gmail.com>
2023-06-15 22:46:25 -0300

    more changes, need to do more

--- valid-protocol/src/components/Home.js
+++ valid-protocol/src/components/Home.js
@@ -9,62 +9,50 @@ export default function Home() {
     
   return (
     <div className='Wrapper'>
-      <div id="hero">
+      <div className="hero">
         <label className="toGreen">Valid</label>
         <label className="green toWhite bold">Protocol</label>
       </div>
-      <div id="smallerText">
+      <div className="smallerText">
         <label className="greenHover bold">Truth</label>, deployed by
         <label className="green bold toWhite">tech.</label>
       </div>
       <div className="subBody">
-        <div className="textSpace" id="connectBody">
+        <div className="textSpace connectBody">
           Connect your <label className="greenHover">Crypto Wallet</label>
-          <div><button id="connectWallet" className="button">Connect</button></div>
+          <div><button className="button">Connect</button></div>
         </div>
-        <div id="textBox">
+        <div className="textBox">
           <div>
             Write below the piece of text you wish to check if is true or false.
           </div>
           <input
-            id="checkText"
+            className="checkText"
             type="text"
             placeholder="     News to be credit-checked"
           />
-          <button id="factButton" className="button">Check</button>
-          <div id="response"></div>
+          <button className="button factButton">Check</button>
+          <div className="response"></div>
         </div>
-        <div id="struct"></div>
+        <div className="struct"></div>
       </div>
       <div className="subBodyTwo">
         <div>
           <div>
-            Our <label className="bold" id="trustText">trustworthy</label> sources*
+            Our <label className="bold trustText">trustworthy</label> sources*
           </div>
-          <div id="firstLineCredited" className="lineCredited">
-            <img
-              src={CNNlogo}
-              alt="CNN Logo"
-              className="imgFormat"
-            />
-            <img src={BBClogo} alt="BBC Logo" className="bbcFormat" />
+          <div className="firstLineCredited lineCredited">
+            <img src={CNNlogo} alt="CNN Logo" className="imgFormat"/>
+            <img src={BBClogo} alt="BBC Logo" className="bbcFormat"/>
           </div>
         </div>
-        <div id="secondLineCredited" className="lineCredited">
-          <img
-            src={NYClogo}
-            alt="NYTimes Logo"
-            className="imgFormat reduceToFifteenPercent"
-          />
-          <img
-            src={WPlogo}
-            alt="Washington Post Logo"
-            className="imgFormat"
-          />
+        <div className="secondLineCredited lineCredited">
+          <img src={NYClogo} alt="NYTimes Logo" className="imgFormat reduceToFifteenPercent"/>
+          <img src={WPlogo} alt="Washington Post Logo" className="imgFormat"/>
         </div>
-        <div id="note">*Purely fictional for now! Just an MVP abstraction.</div>
+        <div className="note">*Purely fictional for now! Just an MVP abstraction.</div>
       </div>
-      <div id="OwnerSpace"></div>
+      <div className="OwnerSpace"></div>
       <script src="./index.js" type="module"></script>
       <script
         src="https://cdn.plot.ly/plotly-2.20.0.min.js"
